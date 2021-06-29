5d6063d8dcd12740d08b13e0e6f3c5e018852581
Chingun Ariunbat <chingun26@yahoo.de>
2021-06-29 16:12:05 +0200

    injects CookieService into SidePreviousPlotsComponent

--- Implementation/frontend/src/app/sidebar/side-previous-plots/side-previous-plots.component.ts
+++ Implementation/frontend/src/app/sidebar/side-previous-plots/side-previous-plots.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from "../../../logic/cookiehandler/cookie.service";
+import { PlotConfiguration } from "../../../logic/plothandler/interfaces/plot-configuration";
 
+/**
+ * It appears as if this component doesn't actually need PlotCardCompenent
+ */
 @Component({
   selector: 'app-side-previous-plots',
   templateUrl: './side-previous-plots.component.html',
@@ -7,9 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidePreviousPlotsComponent implements OnInit {
 
-  constructor() { }
+  recentPlotConfigurations: PlotConfiguration[] = [];
+
+  constructor(private readonly cookieService: CookieService) { }
 
   ngOnInit(): void {
+    this.recentPlotConfigurations = this.cookieService.getRecentPlotConfigurations();
   }
 
 }
