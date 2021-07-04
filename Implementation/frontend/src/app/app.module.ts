a767f81d1349284bb539e9cb46fb81d4dbe688f0
darius1702 <darius1702@gmail.com>
2021-07-04 20:06:20 +0200

    adds HttpClient dependency in rest handler and adjusts modules accordingly

--- Implementation/frontend/src/app/app.module.ts
+++ Implementation/frontend/src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { GitHistoryComponent } from './main-content/dashboard/git-history/git-history.component';
-import { SingleBenchmarkPlotComponent } from './main-content/single-benchmark-plot/single-benchmark-plot.component';
-import { BenchmarkComparisonPlotComponent } from './main-content/benchmark-comparison-plot/benchmark-comparison-plot.component';
-import { AveragePerformanceComponent } from './main-content/dashboard/average-performance/average-performance.component';
-import { PlotConfigurationDialogComponent } from './dialogs/plot-configuration-dialog/plot-configuration-dialog.component';
-import { BenchmarkSelectDialogComponent } from './dialogs/benchmark-select-dialog/benchmark-select-dialog.component';
-import { LoadFromTemplateDialogComponent } from './dialogs/load-from-template-dialog/load-from-template-dialog.component';
-import { ErrorDialogComponent } from './dialogs/error-dialog/error-dialog.component';
-import { BenchmarkCompareDialogComponent } from './dialogs/benchmark-compare-dialog/benchmark-compare-dialog.component';
-import { CookieConsentDialogComponent } from './dialogs/cookie-consent-dialog/cookie-consent-dialog.component';
-import { SideCurrentChosenCommitComponent } from './sidebar/side-current-chosen-commit/side-current-chosen-commit.component';
-import { SideBenchmarkSelectComponent } from './sidebar/side-benchmark-select/side-benchmark-select.component';
-import { SideBenchmarkCompareComponent } from './sidebar/side-benchmark-compare/side-benchmark-compare.component';
-import { SideLoadFromTemplateComponent } from './sidebar/side-load-from-template/side-load-from-template.component';
-import { SidePreviousPlotsComponent } from './sidebar/side-previous-plots/side-previous-plots.component';
-import { PlotCardComponent } from './sidebar/side-previous-plots/plot-card/plot-card.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {GitHistoryComponent} from './main-content/dashboard/git-history/git-history.component';
+import {SingleBenchmarkPlotComponent} from './main-content/single-benchmark-plot/single-benchmark-plot.component';
+import {BenchmarkComparisonPlotComponent} from './main-content/benchmark-comparison-plot/benchmark-comparison-plot.component';
+import {AveragePerformanceComponent} from './main-content/dashboard/average-performance/average-performance.component';
+import {PlotConfigurationDialogComponent} from './dialogs/plot-configuration-dialog/plot-configuration-dialog.component';
+import {BenchmarkSelectDialogComponent} from './dialogs/benchmark-select-dialog/benchmark-select-dialog.component';
+import {LoadFromTemplateDialogComponent} from './dialogs/load-from-template-dialog/load-from-template-dialog.component';
+import {ErrorDialogComponent} from './dialogs/error-dialog/error-dialog.component';
+import {BenchmarkCompareDialogComponent} from './dialogs/benchmark-compare-dialog/benchmark-compare-dialog.component';
+import {CookieConsentDialogComponent} from './dialogs/cookie-consent-dialog/cookie-consent-dialog.component';
+import {SideCurrentChosenCommitComponent} from './sidebar/side-current-chosen-commit/side-current-chosen-commit.component';
+import {SideBenchmarkSelectComponent} from './sidebar/side-benchmark-select/side-benchmark-select.component';
+import {SideBenchmarkCompareComponent} from './sidebar/side-benchmark-compare/side-benchmark-compare.component';
+import {SideLoadFromTemplateComponent} from './sidebar/side-load-from-template/side-load-from-template.component';
+import {SidePreviousPlotsComponent} from './sidebar/side-previous-plots/side-previous-plots.component';
+import {PlotCardComponent} from './sidebar/side-previous-plots/plot-card/plot-card.component';
 import {LibModule} from "../lib/lib.module";
-import { DashboardComponent } from './main-content/dashboard/dashboard.component';
-import { TestComponent } from './main-content/test/test.component';
+import {LogicModule} from "../logic/logic.module";
+import {DashboardComponent} from './main-content/dashboard/dashboard.component';
+import {TestComponent} from './main-content/test/test.component';
 
 @NgModule({
   declarations: [
@@ -52,9 +53,11 @@ import { TestComponent } from './main-content/test/test.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LibModule
+    LibModule,
+    LogicModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ Implementation/frontend/src/logic/logic.module.ts
@@ -0,0 +1,9 @@
+import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+  ]
+})
+export class LogicModule {}
