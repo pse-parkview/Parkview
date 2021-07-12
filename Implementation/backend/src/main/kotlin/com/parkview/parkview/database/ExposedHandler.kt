d1f97014b02e8331a414b09e2945cad6a48ac3ef
Max Schik <max.schik@googlemail.com>
2021-07-12 13:58:46 +0200

    implements batch inserts for database handler

--- Implementation/backend/src/main/kotlin/com/parkview/parkview/database/ExposedHandler.kt
+++ Implementation/backend/src/main/kotlin/com/parkview/parkview/database/ExposedHandler.kt
@@ -1,12 +1,10 @@
 package com.parkview.parkview.database
 
-import com.parkview.parkview.benchmark.BlasBenchmarkResult
-import com.parkview.parkview.benchmark.Format
-import com.parkview.parkview.benchmark.SpmvBenchmarkResult
-import com.parkview.parkview.benchmark.SpmvDatapoint
+import com.parkview.parkview.benchmark.*
 import com.parkview.parkview.git.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.DriverManager
 import java.util.*
 
 private object MatrixBenchmarkResultTable : Table() {
@@ -21,11 +19,11 @@ private object MatrixBenchmarkResultTable : Table() {
     override val primaryKey = PrimaryKey(id, name = "PK_MatrixBenchmarkResult_Id")
 }
 
-private object SpmvFormatTabe : Table() {
+private object SpmvFormatTable : Table() {
     override val tableName: String = "SpmvFormat"
     val id: Column<UUID> = uuid("id").autoGenerate().uniqueIndex()
     val benchmarkId: Column<UUID> = reference("benchmarkId", MatrixBenchmarkResultTable.id)
-    val name: Column<String> = varchar("name", 40) // TODO either use TEXT or check about max format name length
+    val name: Column<String> = varchar("name", 40)
     val completed: Column<Boolean> = bool("completed")
     val time: Column<Double> = double("time")
     override val primaryKey = PrimaryKey(id, name = "PK_SpmvFormat_Id")
@@ -36,7 +34,7 @@ private object BlasBenchmarkResultTable : Table() {
     val id: Column<UUID> = uuid("id").autoGenerate().uniqueIndex()
     val sha: Column<String> = varchar("sha", 40)
     val name: Column<String> = varchar("name", 40)
-    val device: Column<String> = varchar("deviceName", 40) // TODO either use TEXT or check about max device name length
+    val device: Column<String> = varchar("deviceName", 40)
     val n: Column<Long> = long("n")
     val r: Column<Long> = long("r")
     val m: Column<Long> = long("m")
@@ -48,7 +46,7 @@ private object BlasOperationTable : Table() {
     override val tableName: String = "BlasOperation"
     val id: Column<UUID> = uuid("id").autoGenerate().uniqueIndex()
     val benchmarkId: Column<UUID> = reference("benchmarkId", BlasBenchmarkResultTable.id)
-    val name: Column<String> = varchar("name", 40) // TODO either use TEXT or check about max format name length
+    val name: Column<String> = varchar("name", 40)
     val time: Column<Double> = double("time")
     val flops: Column<Double> = double("flops")
     val bandwidth: Column<Double> = double("bandwidth")
@@ -62,90 +60,110 @@ private object BlasOperationTable : Table() {
  */
 class ExposedHandler : DatabaseHandler {
     // TODO: use spring stuff to get database object
-//    val db = Database.connect(
-//        "jdbc:postgresql://parkview-postgres:5432/parkview", driver = "org.postgresql.Driver",
-//        user = "parkview", password = "parkview"
-//    )
-    val db = Database.connect(
-        "jdbc:postgresql://localhost:5432/parkview", driver = "org.postgresql.Driver",
-        user = "parkview", password = "parkview"
-    )
+    // TODO: enable batch insert for database controller
+//    val url = "jdbc:postgresql://parkview-postgres:5432/parkview"
+    private val url = "jdbc:postgresql://localhost:5432/parkview"
+    private val props = Properties()
+        .apply { setProperty("user", "parkview") }
+        .apply { setProperty("password", "parkview") }
+        .apply { setProperty("reWriteBatchedInserts", "true") }
+
+    private val db = Database.connect(getNewConnection = { DriverManager.getConnection(url, props) })
+
 
     init {
-        transaction {
+        transaction(db) {
             addLogger(StdOutSqlLogger) // debug logger
             SchemaUtils.createSchema(Schema("parkview"))
-            SchemaUtils.create(MatrixBenchmarkResultTable, SpmvFormatTabe, BlasBenchmarkResultTable, BlasOperationTable)
+            SchemaUtils.create(
+                MatrixBenchmarkResultTable,
+                SpmvFormatTable,
+                BlasBenchmarkResultTable,
+                BlasOperationTable
+            )
         }
     }
 
     override fun updateBenchmarkResults(results: List<BenchmarkResult>) {
         for (result in results) {
             when (result) {
-                is SpmvBenchmarkResult -> insertSmpvDatapoint(result)
+                is SpmvBenchmarkResult -> insertSpmvDatapoint(result)
                 is BlasBenchmarkResult -> insertBlasBenchmarkResult(result)
             }
         }
     }
 
-    private fun insertSmpvDatapoint(result: SpmvBenchmarkResult) {
-        transaction {
+    private fun insertSpmvDatapoint(result: SpmvBenchmarkResult) {
+        val listOfFormats: List<List<Format>> = result.datapoints.map { it.formats }
+        val allBenchmarkIDs = transaction(db) {
             addLogger(StdOutSqlLogger) // debug logger
-
-            // TODO use a batch insert instead of whatever this is
-            for (datapoint in result.datapoints) {
-                val benchmark = MatrixBenchmarkResultTable.insert {
-                    it[sha] = result.commit.sha
-                    it[name] = result.benchmark.name
-                    it[device] = result.device.name
-                    it[cols] = datapoint.columns
-                    it[rows] = datapoint.rows
-                    it[nonzeros] = datapoint.nonzeros
-                }
-
-                for (format in datapoint.formats) {
-                    SpmvFormatTabe.insert {
-                        it[benchmarkId] = benchmark[MatrixBenchmarkResultTable.id]
-                        it[name] = format.name
-                        it[completed] = format.completed
-                        it[time] = format.time
-                    }
-                }
+            MatrixBenchmarkResultTable.batchInsert(result.datapoints) { datapoint ->
+                this[MatrixBenchmarkResultTable.sha] = result.commit.sha
+                this[MatrixBenchmarkResultTable.name] = result.benchmark.name
+                this[MatrixBenchmarkResultTable.device] = result.device.name
+                this[MatrixBenchmarkResultTable.cols] = datapoint.columns
+                this[MatrixBenchmarkResultTable.rows] = datapoint.rows
+                this[MatrixBenchmarkResultTable.nonzeros] = datapoint.nonzeros
             }
+        }
+
+        val formatsWithId = allBenchmarkIDs.zip(listOfFormats).fold(emptyList<Pair<UUID, Format>>()) { acc, pair ->
+            acc + pair.second.map {
+                Pair(
+                    pair.first[MatrixBenchmarkResultTable.id],
+                    it
+                )
+            }.toList()
+        }
+
+        transaction(db) {
+            addLogger(StdOutSqlLogger) // debug logger
+            SpmvFormatTable.batchInsert(formatsWithId) { (id, format) ->
+                this[SpmvFormatTable.benchmarkId] = id
+                this[SpmvFormatTable.name] = format.name
+                this[SpmvFormatTable.completed] = format.completed
+                this[SpmvFormatTable.time] = format.time
+            }
+
             commit()
         }
     }
 
     private fun insertBlasBenchmarkResult(result: BlasBenchmarkResult) {
-        transaction {
+        val listOfOperations: List<List<Operation>> = result.datapoints.map { it.operations }
+        val allBenchmarkIDs = transaction(db) {
             addLogger(StdOutSqlLogger) // debug logger
-
-            // TODO use a batch insert instead of whatever this is
-            for (datapoint in result.datapoints) {
-                val benchmark = BlasBenchmarkResultTable.insert {
-                    it[sha] = result.commit.sha
-                    it[name] = result.benchmark.name
-                    it[device] = result.device.name
-                    it[n] = datapoint.n
-                    it[r] = datapoint.r
-                    it[m] = datapoint.m
-                    it[k] = datapoint.k
-                }
-
-                println(benchmark[BlasBenchmarkResultTable.id])
-
-                for (operation in datapoint.operations) {
-                    BlasOperationTable.insert {
-                        it[benchmarkId] = benchmark[BlasBenchmarkResultTable.id]
-                        it[name] = operation.name
-                        it[time] = operation.time
-                        it[flops] = operation.flops
-                        it[bandwidth] = operation.bandwidth
-                        it[completed] = operation.completed
-                        it[repetitions] = operation.repetitions
-                    }
-                }
+            BlasBenchmarkResultTable.batchInsert(result.datapoints) { datapoint ->
+                this[BlasBenchmarkResultTable.sha] = result.commit.sha
+                this[BlasBenchmarkResultTable.name] = result.benchmark.name
+                this[BlasBenchmarkResultTable.device] = result.device.name
+                this[BlasBenchmarkResultTable.n] = datapoint.n
+                this[BlasBenchmarkResultTable.r] = datapoint.r
+                this[BlasBenchmarkResultTable.m] = datapoint.m
+                this[BlasBenchmarkResultTable.k] = datapoint.k
             }
+        }
+        val operationsWithId =
+            allBenchmarkIDs.zip(listOfOperations).fold(emptyList<Pair<UUID, Operation>>()) { acc, pair ->
+                acc + pair.second.map {
+                    Pair(
+                        pair.first[MatrixBenchmarkResultTable.id],
+                        it
+                    )
+                }.toList()
+            }
+        transaction(db) {
+            addLogger(StdOutSqlLogger) // debug logger
+            BlasOperationTable.batchInsert(operationsWithId) { (id, operation) ->
+                this[BlasOperationTable.benchmarkId] = id
+                this[BlasOperationTable.name] = operation.name
+                this[BlasOperationTable.completed] = operation.completed
+                this[BlasOperationTable.time] = operation.time
+                this[BlasOperationTable.flops] = operation.flops
+                this[BlasOperationTable.bandwidth] = operation.bandwidth
+                this[BlasOperationTable.repetitions] = operation.repetitions
+            }
+
             commit()
         }
     }
@@ -166,7 +184,7 @@ class ExposedHandler : DatabaseHandler {
     }
 
     private fun fetchSpmvResult(
-        commitInfo: Commit,
+        commit: Commit,
         device: Device,
         benchmarkType: Benchmark,
         rowLim: Int,
@@ -174,9 +192,9 @@ class ExposedHandler : DatabaseHandler {
         nonzerosLim: Int
     ): BenchmarkResult {
         val datapoints = mutableListOf<SpmvDatapoint>()
-        val benchmarks = transaction {
+        val benchmarks = transaction(db) {
             MatrixBenchmarkResultTable.select {
-                (MatrixBenchmarkResultTable.sha eq commitInfo.sha) and
+                (MatrixBenchmarkResultTable.sha eq commit.sha) and
                         (MatrixBenchmarkResultTable.device eq device.name) and
                         (MatrixBenchmarkResultTable.name eq benchmarkType.name) and
                         (MatrixBenchmarkResultTable.rows greaterEq rowLim) and
@@ -185,28 +203,26 @@ class ExposedHandler : DatabaseHandler {
             }.toList()
         }
 
-        // TODO: make this faster
         datapoints += benchmarks.map { spmvEntry ->
             SpmvDatapoint(
                 rows = spmvEntry[MatrixBenchmarkResultTable.rows],
                 columns = spmvEntry[MatrixBenchmarkResultTable.cols],
                 nonzeros = spmvEntry[MatrixBenchmarkResultTable.nonzeros],
-                formats = transaction {
-                    SpmvFormatTabe
-                        .select { SpmvFormatTabe.benchmarkId eq spmvEntry[MatrixBenchmarkResultTable.id] }
+                formats = transaction(db) {
+                    SpmvFormatTable
+                        .select { SpmvFormatTable.benchmarkId eq spmvEntry[MatrixBenchmarkResultTable.id] }
                         .map { formatEntry ->
                             Format(
-                                name = formatEntry[SpmvFormatTabe.name],
-                                time = formatEntry[SpmvFormatTabe.time],
-                                completed = formatEntry[SpmvFormatTabe.completed],
+                                name = formatEntry[SpmvFormatTable.name],
+                                time = formatEntry[SpmvFormatTable.time],
+                                completed = formatEntry[SpmvFormatTable.completed],
                             )
                         }
                 }
             )
         }
-        println(datapoints)
         return SpmvBenchmarkResult(
-            commit = commitInfo,
+            commit = commit,
             device = device,
             benchmark = benchmarkType,
             datapoints = datapoints
