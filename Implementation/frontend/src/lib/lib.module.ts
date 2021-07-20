d44eea589e02ec5db2660306075ff27d7e89cc86
Chingun Ariunbat <chingun26@yahoo.de>
2021-07-21 00:29:31 +0200

    creates a summary card stub

--- Implementation/frontend/src/lib/lib.module.ts
+++ Implementation/frontend/src/lib/lib.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import {NgxChartsModule} from "@swimlane/ngx-charts";
 import { DefaultvaluePipe } from './pipes/defaultvalue.pipe';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
+import { SummaryCardComponent } from './directives/summary-card/summary-card.component';
+import {MatCardModule} from "@angular/material/card";
+import {AppModule} from "../app/app.module";
+import {MatTableModule} from "@angular/material/table";
 import {AbbreviatePipe} from "./pipes/abbreviate.pipe";
 
 
 
 @NgModule({
   declarations: [
+    DefaultvaluePipe,
+    SummaryCardComponent,
     AbbreviatePipe,
     DefaultvaluePipe,
   ],
@@ -20,6 +26,8 @@ import {AbbreviatePipe} from "./pipes/abbreviate.pipe";
     CommonModule,
     NgxChartsModule,
     MatSnackBarModule,
+    MatCardModule,
+    MatTableModule,
   ]
 })
 export class LibModule { }
