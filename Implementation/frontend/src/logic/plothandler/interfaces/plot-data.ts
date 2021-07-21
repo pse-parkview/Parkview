2c6bb5d88cc4ab076d6ba06c8a7e604e0b283659
Chingun Ariunbat <chingun26@yahoo.de>
2021-07-21 22:33:07 +0200

    Adjusts Bar Plot to correspond to the new PlotData Format

--- Implementation/frontend/src/logic/plothandler/interfaces/plot-data.ts
+++ Implementation/frontend/src/logic/plothandler/interfaces/plot-data.ts
@@ -1,6 +1,7 @@
 import {ChartDataSets} from "chart.js";
+import {Label} from "ng2-charts";
 
 export interface PlotData {
   get datasets(): ChartDataSets[];
-  get labels(): string[];
+  get labels(): Label[];
 }
