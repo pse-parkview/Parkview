d01a0797bb0902cd7676b4d889acff9e0b3dcf21
Chingun Ariunbat <chingun26@yahoo.de>
2021-08-04 21:44:45 +0200

    re-implements templates as intended

--- Implementation/frontend/src/app/sidebar/side-load-from-template/side-load-from-template.component.ts
+++ Implementation/frontend/src/app/sidebar/side-load-from-template/side-load-from-template.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {LoadFromTemplateDialogComponent} from "../../dialogs/load-from-template-dialog/load-from-template-dialog.component";
+import {CookieService} from "../../../logic/cookiehandler/cookie.service";
+import {Observer} from "rxjs";
+import {PlotConfiguration} from "../../../logic/plothandler/interfaces/plot-configuration";
+import {SelectionService} from "../../../logic/commit-selection-handler/selection.service";
+import {DataService} from "../../../logic/datahandler/data.service";
+import {PlotTypeOption} from "../../../logic/plothandler/interfaces/available-plot-types";
 
 @Component({
   selector: 'app-side-load-from-template',
@@ -9,15 +13,34 @@ import {LoadFromTemplateDialogComponent} from "../../dialogs/load-from-template-
 })
 export class SideLoadFromTemplateComponent implements OnInit {
 
-  constructor(private readonly matDialog: MatDialog) {
+  usableTemplates: PlotConfiguration[] = [];
+
+  private readonly templateViewUpdater: Observer<void> = {
+    next: () => {
+      const allTemplates = this.cookieService.getSavedTemplates();
+      const selectedCommits = this.commitSelectionService.getSelectedCommits();
+      const commits = selectedCommits.commitsAndDevices.map(p => p.commit);
+      const devices = selectedCommits.commitsAndDevices.map(p => p.device);
+      this.dataService.getAvailablePlots(selectedCommits.benchmarkName, commits, devices).subscribe((plotTypeOptions: PlotTypeOption[]) => {
+        this.usableTemplates = allTemplates.filter(plotConfig => plotTypeOptions.some(pto => pto.plotName == plotConfig.plotType));
+      })
+    },
+    error: (err) => {
+      console.error(err);
+    },
+    complete: () => {
+      // do nothing
+    },
+  }
+
+  constructor(private readonly commitSelectionService: SelectionService,
+              private readonly cookieService: CookieService,
+              private readonly dataService: DataService) {
     // only suitable for DI
   }
 
   ngOnInit(): void {
-    // no initialiation necessary
-  }
-
-  spawnTemplateDialog(): void {
-    this.matDialog.open(LoadFromTemplateDialogComponent);
+    this.cookieService.savedTemplateUpdate.subscribe(this.templateViewUpdater);
+    this.commitSelectionService.selectedCommitsHasUpdated.subscribe(this.templateViewUpdater);
   }
 }
