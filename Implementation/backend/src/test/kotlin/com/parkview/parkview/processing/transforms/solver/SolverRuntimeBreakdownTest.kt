81a04e075468d57e7988bea26aa3a860505fa72c
Max Schik <max.schik@googlemail.com>
2021-08-19 02:11:18 +0200

    adds test for SolverRuntimeBreakdown

---
+++ Implementation/backend/src/test/kotlin/com/parkview/parkview/processing/transforms/solver/SolverRuntimeBreakdownTest.kt
@@ -0,0 +1,99 @@
+package com.parkview.parkview.processing.transforms.solver
+
+import COMMIT_A
+import DEVICE
+import com.parkview.parkview.benchmark.Component
+import com.parkview.parkview.benchmark.Solver
+import com.parkview.parkview.benchmark.SolverBenchmarkResult
+import com.parkview.parkview.benchmark.SolverDatapoint
+import com.parkview.parkview.git.BenchmarkType
+import com.parkview.parkview.processing.transforms.BarChartDataset
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class SolverRuntimeBreakdownTest {
+    private val plot = SolverRuntimeBreakdown()
+
+    private val result = SolverBenchmarkResult(
+        COMMIT_A,
+        DEVICE,
+        listOf(
+            SolverDatapoint(
+                "name",
+                1,
+                1,
+                1,
+                "optimal",
+                listOf(
+                    Solver(
+                        name = "solver",
+                        completed = true,
+                        applyComponents = listOf(
+                            Component("a", 50.0),
+                            Component("b", 50.0),
+                        ),
+                        generateComponents = listOf(
+                            Component("a", 75.0),
+                            Component("b", 25.0),
+                        ),
+                        applyTotalTime = 200.0,
+                        generateTotalTime = 200.0,
+                        applyIterations = 1,
+                    ),
+                ),
+            )
+        )
+    )
+
+    @Test
+    fun `test apply sumComponents`() {
+        val options = mapOf(
+            "matrix" to "name",
+            "components" to "apply",
+            "totalTime" to "sumComponents",
+        )
+        val data = plot.transformSolver(listOf(result), options)
+
+        assertEquals(0.5, (data.datasets[0] as BarChartDataset).data[0], 0.0001)
+        assertEquals(0.5, (data.datasets[1] as BarChartDataset).data[0], 0.0001)
+    }
+
+    @Test
+    fun `test apply givenValue`() {
+        val options = mapOf(
+            "matrix" to "name",
+            "components" to "apply",
+            "totalTime" to "givenValue",
+        )
+        val data = plot.transformSolver(listOf(result), options)
+
+        assertEquals(0.25, (data.datasets[0] as BarChartDataset).data[0], 0.0001)
+        assertEquals(0.25, (data.datasets[1] as BarChartDataset).data[0], 0.0001)
+    }
+
+    @Test
+    fun `test generate sumComponents`() {
+        val options = mapOf(
+            "matrix" to "name",
+            "components" to "generate",
+            "totalTime" to "sumComponents",
+        )
+        val data = plot.transformSolver(listOf(result), options)
+
+        assertEquals(0.75, (data.datasets[0] as BarChartDataset).data[0], 0.0001)
+        assertEquals(0.25, (data.datasets[1] as BarChartDataset).data[0], 0.0001)
+    }
+
+    @Test
+    fun `test generate givenValue`() {
+        val options = mapOf(
+            "matrix" to "name",
+            "components" to "generate",
+            "totalTime" to "givenValue",
+        )
+        val data = plot.transformSolver(listOf(result), options)
+
+        assertEquals(0.375, (data.datasets[0] as BarChartDataset).data[0], 0.0001)
+        assertEquals(0.125, (data.datasets[1] as BarChartDataset).data[0], 0.0001)
+    }
+}
