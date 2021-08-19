3370bbd2b86e509cafed5bb2906eba29a5ca7cb9
Max Schik <max.schik@googlemail.com>
2021-08-19 11:49:26 +0200

    adds test for SpmvSpeedupTransform

---
+++ Implementation/backend/src/test/kotlin/com/parkview/parkview/processing/transforms/spmv/SpmvSpeedupPlotTest.kt
@@ -0,0 +1,28 @@
+package com.parkview.parkview.processing.transforms.spmv
+
+import SPMV_RESULT
+import com.parkview.parkview.processing.transforms.PointDataset
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SpmvSpeedupPlotTest {
+    private val plot = SpmvSpeedupPlot()
+
+    private val benchmark = SPMV_RESULT
+
+    @Test
+    fun transformSpmv() {
+        val options = mutableMapOf(
+            "compare" to "${benchmark.identifier}/${benchmark.identifier}",
+            "xAxis" to "rows",
+        )
+        val data = plot.transformSpmv(listOf(benchmark, benchmark), options)
+
+        for (dataset in data.datasets) {
+            for (point in (dataset as PointDataset).data) {
+                assertEquals(1.0, point.y, 0.0001)
+            }
+        }
+    }
+}
