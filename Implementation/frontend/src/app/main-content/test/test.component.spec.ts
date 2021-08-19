24e1086ade019248ccd3cce96e24cb9585477cdc
Darius Schefer <darius1702@gmail.com>
2021-08-19 15:40:08 +0200

    'cleanup'

--- Implementation/frontend/src/app/main-content/test/test.component.spec.ts
+++ Implementation/frontend/src/app/main-content/test/test.component.spec.ts
@@ -2,7 +2,6 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {TestComponent} from './test.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
-import {MatDialogModule} from "@angular/material/dialog";
 
 describe('TestComponent', () => {
   let component: TestComponent;
