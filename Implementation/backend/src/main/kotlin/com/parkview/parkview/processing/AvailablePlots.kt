58b8cf348fd0ff67cba7e796bc3d2e8c9c8b968c
jooe1 <57239467+jooe1@users.noreply.github.com>
2021-10-01 13:53:42 +0200

    fixed import

--- Implementation/backend/src/main/kotlin/com/parkview/parkview/processing/AvailablePlots.kt
+++ Implementation/backend/src/main/kotlin/com/parkview/parkview/processing/AvailablePlots.kt
@@ -9,6 +9,7 @@ import com.parkview.parkview.processing.transforms.blas.SingleBlasPlot
 import com.parkview.parkview.processing.transforms.matrix.conversion.ConversionPlotTransform
 import com.parkview.parkview.processing.transforms.matrix.conversion.ConversionSingleScatterPlot
 import com.parkview.parkview.processing.transforms.matrix.conversion.ConversionSpeedupPlot
+import com.parkview.parkview.processing.transforms.matrix.preconditioner.PreconditionerOverview
 import com.parkview.parkview.processing.transforms.matrix.preconditioner.PreconditionerPlotTransform
 import com.parkview.parkview.processing.transforms.matrix.solver.SolverConvergencePlot
 import com.parkview.parkview.processing.transforms.matrix.solver.SolverPlotTransform
--- Implementation/backend/src/main/kotlin/com/parkview/parkview/rest/ParkviewApiHandler.kt
+++ Implementation/backend/src/main/kotlin/com/parkview/parkview/rest/ParkviewApiHandler.kt
@@ -35,12 +35,6 @@ class ParkviewApiHandler(
 
     }
 
-    private fun hash(input: String) : String{
-        val md = MessageDigest.getInstance("MD5")
-        return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32,'0')
-    }
-
-
     override fun getHistory(
         branch: String,
         page: Int,
