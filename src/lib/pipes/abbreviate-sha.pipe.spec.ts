c2cea7dbb6b87eca7f8b8b3e49f71e4a1ef0cffc
Darius Schefer <darius1702@gmail.com>
2022-04-08 20:17:20 +0200

    remove old test scripts

--- src/lib/pipes/abbreviate-sha.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {AbbreviateShaPipe} from './abbreviate-sha.pipe';
-
-describe('AbbreviateShaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AbbreviateShaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/lib/pipes/defaultvalue.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {DefaultvaluePipe} from './defaultvalue.pipe';
-
-describe('DefaultvaluePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DefaultvaluePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
