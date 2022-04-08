b37ac68d7875b59969c73501bc12d09c0f1a9ed1
Darius Schefer <45409137+darius1702@users.noreply.github.com>
2022-04-08 21:34:15 +0200

    Remove old test scripts (#129)
    * remove old test scripts
    
    * Make karma accept empty tests
    
    * Update ci.yml
    
    Co-authored-by: Maximilian Schik <max.schik@googlemail.com>

--- src/lib/pipes/date-formatter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {DateFormatterPipe} from './date-formatter.pipe';
-
-describe('DateFormatterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateFormatterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
